0ecee93a9c33416e674818d8d38169af656efecb
angelbr09 <109047516+angelbr09@users.noreply.github.com>
2022-07-10 21:07:37 -0500

    cambio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { InicioModule } from './modules/inicio/inicio.module';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from './shared/shared.module';
 import {MatFormFieldModule} from "@angular/material/form-field";
 
 @NgModule({
@@ -19,8 +18,8 @@ import {MatFormFieldModule} from "@angular/material/form-field";
     CoreModule,
     InicioModule,
     NoopAnimationsModule,
-    SharedModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+
 
   ],
   providers: [],
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FooterBotComponent} from "./footer/footer/footer-bot/footer-bot.component";
+import {HeaderTopComponent} from "./header/header-top/header-top.component";
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FooterBotComponent,
+    HeaderTopComponent
+  ],
+  exports: [
+    FooterBotComponent,
+    HeaderTopComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
   ]
 })
 export class CoreModule { }
